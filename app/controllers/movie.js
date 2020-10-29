30e397f4bc7d0fee18092bb478cbb063b7c54d51
fadelmajid <fadelmajid@gmail.com>
2020-10-29 08:12:43 +0700

    test

--- app/controllers/movie.js
+++ app/controllers/movie.js
@@ -4,6 +4,7 @@ const _ = require('underscore')
 const request = require('axios')
 const config = require('../../config/config.json')
 const validator = require('validator')
+const movie = require('../routes/movie')
 
 let obj = (rootpath) => {
     const fn = {}
@@ -13,6 +14,7 @@ let obj = (rootpath) => {
             let search = req.query.s || ''
             let p = req.query.page || 1
 
+            console.log(search)
             // validate search
             if(validator.isEmpty(search)){
                 throw getMessage('Search tidak boleh kosong')
@@ -34,52 +36,43 @@ let obj = (rootpath) => {
                 throw getMessage('Not found')
             }
 
+            // log to database
+
             res.success(result.data)
         } catch (e) {next(e)}
     }
 
-    fn.getAllMovie = async (req, res, next) => {
+
+    fn.getMovie = async (req, res, next) => {
         try {
-            let user_id = parseInt(req.objUser.user_id) || 0
-            if (user_id <= 0) {
-                throw getMessage('usr006')
+            let movie_id = req.query.i || ''
+            let title = req.query.t || ''
+
+            // validate search
+            if(validator.isEmpty(movie_id) && validator.isEmpty(title)){
+                throw getMessage('Movie ID / title tidak boleh kosong')
             }
 
-            let keyword = req.query.keyword || ''
-            keyword = '%' + keyword + '%'
-            let where = ' AND is_deleted = $1 AND user_id = $2 AND (udata_username LIKE $3 OR udata_account LIKE $4) '
-            let data = [false, user_id, keyword, keyword]
-            let order_by = ' udata_id ASC '
-            let result = await req.model('user').getAllUserData(where, data, order_by)
-
-            res.success(result)
-        } catch(e) {next(e)}
-    }
-
-    fn.getPagingMovie = async (req, res, next) => {
-        try {
-            let user_id = parseInt(req.objUser.user_id) || 0
-            if (user_id <= 0) {
-                throw getMessage('usr006')
+            let data = {
+                url: config.movie_url,
+                options: {
+                    headers: {
+                        'Content-Type': 'application/json;'
+                    }
+                }
             }
 
-            let keyword = req.query.keyword || ''
-            keyword = '%' + keyword + '%'
-            let where = ' AND is_deleted = $1 AND user_id = $2 AND (udata_username LIKE $3 OR udata_account LIKE $4) '
-            let data = [false, user_id, keyword, keyword]
-            let order_by = ' udata_id ASC '
-            let page_no = req.query.page || 0
-            let no_per_page = req.query.perpage || 0
-            let result = await req.model('user').getPagingUserData(
-                where,
-                data,
-                order_by,
-                page_no,
-                no_per_page
-            )
+            let result = await request.get(`${data.url}?apikey=${config.api_key}&i=${movie_id}&t=${title}`, data.options);
+            
+            // validate response
+            if(result.data.Response == "False"){
+                throw getMessage('Not found')
+            }
 
-            res.success(result)
-        } catch(e) {next(e)}
+            // log to database
+
+            res.success(result.data)
+        } catch (e) {next(e)}
     }
 
     return fn;
--- app/routes/movie.js
+++ app/routes/movie.js
@@ -5,7 +5,8 @@ module.exports = (app) => {
 
     let aRoutes = [
         // START DATA
-        {method: 'get', route: '/search', inits: [], middlewares: [movieController.searchMovie], auth: 'no'},
+        {method: 'get', route: '/search', inits: [], middlewares: [movieController.searchMovie], auth: 'login'},
+        {method: 'get', route: '/detail', inits: [], middlewares: [movieController.getMovie], auth: 'login'},
         // {method: 'get', route: '/data/all', inits: [], middlewares: [movieController.getAllMovie], auth: 'login'},
         // {method: 'get', route: '/data', inits: [], middlewares: [movieController.getPagingMovie], auth: 'no'},
     ]
